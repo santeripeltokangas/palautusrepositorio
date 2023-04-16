849eb612a5869e6151de61c8ac056d6c6ff6873a
santeripeltokangas <santeripe@gmail.com>
2023-04-16 21:18:30 +0300

    Maiden tiedot tehty

--- osa2/puhelinluettelo/src/osat/Persons.js
+++ osa2/puhelinluettelo/src/osat/Persons.js
@@ -1,14 +1,13 @@
 const Persons = ({ persons, nameFilter, onDelete }) => {
-    const filteredPersons = persons.filter((person) =>
+    const filteredPeople = persons.filter((person) =>
       person.name.toLowerCase().includes(nameFilter.toLowerCase())
-    );
-  
-    return filteredPersons.map((person) => (
+    )
+    return filteredPeople.map((person) => (
       <div key={person.name}>
         {person.name} {person.number}{' '}
         <button onClick={() => onDelete(person)}>Poista</button>
       </div>
-    ));
-  };
+    ))
+  }
   
-  export default Persons;
+  export default Persons
